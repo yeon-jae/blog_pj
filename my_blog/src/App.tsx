4078b5f2d2c821974107d2597e50430af23cca66
yeon-jae <lisanam99@naver.com>
2024-03-21 14:56:50 +0900

    change_App

--- my_blog/src/App.tsx
+++ my_blog/src/App.tsx
@@ -5,42 +5,5 @@ import "./App.css";
 
 function App() {
   return <Router />;
-=======
-import "./App.css";
-import { Route, Routes, Navigate, Link } from "react-router-dom";
-function App() {
-  <>
-    <ul>
-      <li>
-        <Link to="/">Home</Link>
-      </li>
-      <li>
-        <Link to="/posts">Post List</Link>
-      </li>
-      <li>
-        <Link to="/posts/:id">Post Detail</Link>
-      </li>
-      <li>
-        <Link to="/posts/new">Post New</Link>
-      </li>
-      <li>
-        <Link to="/posts/edit/:id">Post Edit</Link>
-      </li>
-      <li>
-        <Link to="/profile">Profile</Link>
-      </li>
-    </ul>
-  </>;
-  return (
-    <Routes>
-      <Route path="/" element={<h1>home page</h1>} />
-      <Route path="/posts" element={<h1>Post list Page</h1>} />
-      <Route path="/posts/:id" element={<h1>Post Detail Page</h1>} />
-      <Route path="/posts/new" element={<h1>Post New Page</h1>} />
-      <Route path="/posts/edit/:id" element={<h1>Post Edit Page</h1>} />
-      <Route path="/profile" element={<h1>Profile Page</h1>} />
-    </Routes>
-  );
->>>>>>> d7e888f1ffbbc0074ff52b356a594297634c189e
 }
 export default App;
