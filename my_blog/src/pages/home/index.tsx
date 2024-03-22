5cb6db488ff10ac39fedcba335601908214ce29b
yeon-jae <lisanam99@naver.com>
2024-03-22 15:30:58 +0900

    feat: create board

--- my_blog/src/pages/home/index.tsx
+++ my_blog/src/pages/home/index.tsx
@@ -1,3 +1,27 @@
+import { Link } from "react-router-dom";
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <div>
+      <header>
+        <div>
+          <Link to="/posts/new">글쓰기</Link>
+          <Link to="/posts">게시글</Link>
+          <Link to="/profile">프로필</Link>
+        </div>
+      </header>
+
+      <div className="post__list">
+        {[...Array(10)].map((e, index) => (
+          <div key={index} className="post__box">
+            게시글{index}
+          </div>
+        ))}
+      </div>
+      <footer>
+        <div>menu1</div>
+        <div>menu2</div>
+        <div>menu3</div>
+      </footer>
+    </div>
+  );
 }
