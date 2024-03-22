744c3e765f1f3d3b6f63795de0b01de57d1a5d65
yeon-jae <lisanam99@naver.com>
2024-03-22 15:50:34 +0900

    feat: make_blog_activate

--- my_blog/src/pages/home/index.tsx
+++ my_blog/src/pages/home/index.tsx
@@ -9,18 +9,39 @@ export default function Home() {
           <Link to="/profile">프로필</Link>
         </div>
       </header>
-
+      <div className="post__navigation">
+        <div className="post__navigation--active">전체 글</div>
+        <div>나의 글</div>
+      </div>
       <div className="post__list">
         {[...Array(10)].map((e, index) => (
           <div key={index} className="post__box">
-            게시글{index}
+            <Link to={`/posts/${index}`}>
+              {/**프로필 만들기 */}
+              <div className="post__profile-box">
+                <div className="post__profile"></div>
+                <div className="post__author-name">글쓴이</div>
+                <div className="post_date">2024.03.22</div>
+              </div>
+              <div className="post__title"> 게시글{index}</div>
+              <div className="post__text">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Temporibus earum culpa excepturi quas? Officiis inventore optio
+                unde rerum natus possimus, ipsam dolorem nemo ut et omnis dicta!
+                Est repellat eligendi placeat consequuntur possimus. Perferendis
+                quis quo, expedita illo deleniti ipsum quae et error laboriosam,
+                placeat enim voluptas architecto ipsam. Commodi.
+              </div>
+              <div className="post__edit">수정</div>
+              <div className="post__delete">삭제</div>
+            </Link>
           </div>
         ))}
       </div>
       <footer>
-        <div>menu1</div>
-        <div>menu2</div>
-        <div>menu3</div>
+        <Link to="/posts/new">menu1</Link>
+        <Link to="/posts">menu2</Link>
+        <Link to="/profile">menu3</Link>
       </footer>
     </div>
   );
