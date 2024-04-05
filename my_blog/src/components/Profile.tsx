bdd1621fae130419d3befae032f94ef5373f0001
yeon-jae <lisanam99@naver.com>
2024-04-05 15:41:50 +0900

    firebase_logout

--- my_blog/src/components/Profile.tsx
+++ my_blog/src/components/Profile.tsx
@@ -1,18 +1,32 @@
-import { Link } from "react-router-dom";
-
+import { app } from "firebaseApp";
+import { getAuth, signOut } from "firebase/auth";
+import { toast } from "react-toastify";
 export default function Profile() {
+  const onSingOut = async () => {
+    try {
+      const auth = getAuth(app);
+      await signOut(auth);
+      toast.success("로그아웃 되었습니다.");
+    } catch (error: any) {
+      console.log(error);
+      toast.error(error?.code);
+    }
+  };
+  const auth = getAuth(app);
   return (
     <div className="profile__box">
       <div className="flex__box-lg">
         <div className="profile__image" />
         <div>
-          <div className="profile__email">lisanamyj99@gmail.com</div>
-          <div className="profile__name">광나루맛zip</div>
+          <div className="profile__email">{auth?.currentUser?.email}</div>
+          <div className="profile__name">
+            {auth?.currentUser?.displayName || "사용자"}
+          </div>
         </div>
       </div>
-      <Link to="/" className="profile__logout">
+      <div role="presentation" className="profile__logout" onClick={onSingOut}>
         로그아웃
-      </Link>
+      </div>
     </div>
   );
 }
